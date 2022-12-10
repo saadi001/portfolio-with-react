fad8dfef90d35fffa0c769d4f1f74e2504cc1b21
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-10 21:14:30 +0600

    navbar added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from '../pages/Home/Home';
 
 const Main = () => {
      return (
           <div>
-               this is main layout
+               <Home></Home>
           </div>
      );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../navbar/Navbar';
 
 const Home = () => {
      return (
           <div>
-               this is home page
+               <Navbar></Navbar>
           </div>
      );
 };
