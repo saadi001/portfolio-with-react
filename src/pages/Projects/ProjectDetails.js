5173eadecb8615d4d2c1f97357f449ec76d03573
Sheikh sadi <sheikhsadi1620@gmail.com>
2022-12-25 01:15:57 +0600

    project details updated

--- src/pages/Projects/ProjectDetails.js
+++ src/pages/Projects/ProjectDetails.js
@@ -1,42 +1,53 @@
 
-import {useLoaderData} from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import TechnologyDetail from './TechnologyDetail';
+import browser from '../../assset/icons/globe-svgrepo-com.svg'
+import github from '../../assset/icons/icons8-github (1).svg'
 
 const ProjectDetails = () => {
-    
+
      const project = useLoaderData();
-     const {name,image,details,shortDescription,technology} = project;
-     console.log(project )
+     const { name, image, details, shortDescription, technology, liveLink, clientSide, serverSide } = project;
+     console.log(project)
      return (
           <div>
                <div className='bg-[#15202B] min-h-screen'>
-                    <div className='w-full container mx-auto py-6 md:py-10 px-4'>                                                       
-                              <div className='flex flex-col pb-5'>
-                                   <div className='text-teal-500 text-3xl md:text-5xl pb-4 font-semibold font-mono'>{name}</div>
-                                   <div><img className='w-full md:w-1/2 rounded-md' src={image} alt="" /></div>
-                              </div>
-                              <div className='w-full md:w-1/2 flex justify-between items-center flex-wrap'>
-                                   <p className='text-teal-500 text-2xl mb-3'>Overview: </p>
-                                   <p className='text-slate-200'>-{shortDescription}</p>
-                              </div>
-                              <div className='mt-5'>
-                                   <p className='text-teal-500 text-2xl mb-3'>Features:</p>
+                    <div className='w-full container mx-auto py-6 md:py-10 px-4'>
+                         <div className='flex flex-col pb-5'>
+                              <div className='text-teal-500 text-3xl md:text-5xl pb-4 font-semibold font-mono'>{name}</div>
+                              <div><img className='w-full md:w-1/2 rounded-md' src={image} alt="" /></div>
+                         </div>
+                         <div className='flex justify-center w-full md:w-1/2 gap-5 my-3'>
+                              <a title='live link' href={liveLink} target='_blank' rel='noreferrer' ><img className='w-10' src={browser} alt="" /></a>
+                              <a title='client side code' href={clientSide} target='_blank' rel='noreferrer'><img className='w-10' src={github} alt="" /></a>
+                              <a title='server side code' href={serverSide} target='_blank' rel='noreferrer'><img className='w-10' src={github} alt="" /></a>
+                         </div>
+                         <div className='w-full md:w-1/2 flex justify-between items-center flex-wrap'>
+                              <p className='text-teal-500 text-2xl mb-3'>Overview: </p>
+                              <p className='text-slate-200'>-{shortDescription}</p>
+                         </div>
+                         <div className='mt-5'>
+                              <p className='text-teal-500 text-2xl mb-3'>Features:</p>
+                              {
+                                   details.map((detail, i) => <li key={i} className='text-slate-200 pl-2'>{detail.option}</li>)
+                              }
+                         </div>
+                         <div className='my-5'>
+                              <p className='text-teal-500 text-2xl'>Technology used:</p>
+                              <div className='grid grid-cols-1 md:grid-cols-2  gap-5 mt-4'>
                                    {
-                                        details.map((detail,i) => <li key={i} className='text-slate-200 pl-2'>{detail.option}</li>)
+                                        technology.map((tech, i) => <TechnologyDetail
+                                             key={i}
+                                             tech={tech}
+                                        ></TechnologyDetail>)
                                    }
                               </div>
-                              <div className='my-5'>
-                                   <p className='text-teal-500 text-2xl'>Technology used:</p>
-                                   <div className='grid grid-cols-1 md:grid-cols-2  gap-5 mt-4'>
-                                       {
-                                        technology.map((tech,i) => <TechnologyDetail
-                                        key={i}
-                                        tech={tech}
-                                        ></TechnologyDetail>)
-                                       } 
-                                   </div>
-                              </div>
+                         </div>
+                         <div>
+                              back
+                         </div>
                     </div>
+
                </div>
           </div>
      );
